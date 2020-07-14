e4be9161e66312176e1b2c032830e71a0639019c
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-07-14 14:16:47 -0400

    agregada la documentación de atributos y métodos

--- src/main/java/cl/leid/detta/controladores/LoginController.java
+++ src/main/java/cl/leid/detta/controladores/LoginController.java
@@ -2,6 +2,10 @@ package cl.leid.detta.controladores;
 
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -16,16 +20,33 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(path = "/")
 public class LoginController {
 
+    // Constantes
+    // -----------------------------------------------------------------------------------------
+
+    /** Objeto {@link Logger} con los métodos para hacer depuración */
+    private static final Logger LOG = LoggerFactory.getLogger(LoginController.class);
+
     // Atributos
     // -----------------------------------------------------------------------------------------
 
-    /** Objeto {@link MessageSource} */
+    /**
+     * Objeto {@link MessageSource} que contiene los métodos de obtención de textos
+     * de la aplicación
+     */
     @Autowired
     private MessageSource messageSource;
 
     // Solicitudes GET
     // -----------------------------------------------------------------------------------------
 
+    /**
+     * Muestra el formulario de inicio de sesión, siempre y cuando el usuario no
+     * esté autenticado
+     * 
+     * @param locale objeto {@link Locale} con la información regional del cliente
+     * @return un objeto {@link ModelAndView} con el modelo y la vista de la
+     *         respuesta
+     */
     @GetMapping(path = "/login")
     public ModelAndView mostrarFormulario(Locale locale) {
         // Obtener objeto de autenticación
@@ -46,9 +67,29 @@ public class LoginController {
         // Devolver modeloVista
         return modeloVista;
     }
-    
+
+    /**
+     * Muestra al usuario la página de acceso denegado cuando no tiene las
+     * credenciales necesarias para ejecutar una acción en particular
+     * 
+     * @param request objeto {@link HttpServletRequest} con la información de la
+     *                solicitud que le hace el cliente al {@link HttpServlet}
+     * @param locale  objeto {@link Locale} con la información regional del cliente
+     * @return un objeto {@link ModelAndView} con el modelo y la vista de la
+     *         respuesta
+     */
     @GetMapping(path = "/denied")
-    public ModelAndView permisoDenegado(Locale locale) {
+    public ModelAndView permisoDenegado(HttpServletRequest request, Locale locale) {
+        // Obtener autenticación
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        // Verificar autenticación
+        if (auth != null) {
+            // Mostrar acción por consola
+            LOG.warn("Usuario: '" + auth.getName() + "' intentó acceder a la sección protegida: "
+                    + request.getContextPath());
+        }
+
         // Crear nuevo modelo-vista
         ModelAndView modeloVista = new ModelAndView("denied");
 
