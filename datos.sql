44ba15e38eac793db969866d79b15d75f9eaacc7
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-07-22 22:29:00 -0400

    agregados los datos para detta_usuarios y detta_usuarios_roles

---
+++ datos.sql
@@ -0,0 +1,19 @@
+----------------------------------------------------------------------------------------------------
+-- Tabla: detta_usuarios
+----------------------------------------------------------------------------------------------------
+
+INSERT INTO detta_usuarios (email, password, enabled) VALUES ('admin@detta.cl', '$2a$10$YW.QNtpYrJouwMMrmlT28OPxbXzxL2gUGFmbg8THUXaYXRKmcqrjW', '1');
+INSERT INTO detta_usuarios (email, password, enabled) VALUES ('profesional.uno@detta.cl', '$2a$10$JZvFcnU.ku4sDHCtBjGIOetcK506a0kq5KzvfTkTe7DNYlDKfwbsq', '1');
+INSERT INTO detta_usuarios (email, password, enabled) VALUES ('profesional.dos@detta.cl', '$2a$10$WfDYhKcRY/QxPAldeES6Q.ih5zSO9GwyNrCjkBWSU6QzLnzXAWjT2', '1');
+INSERT INTO detta_usuarios (email, password, enabled) VALUES ('profesional.tres@detta.cl', '$2a$10$bEjiVk9QTYyYrgW16ejyKeYwCeEB5jJuwKkzBqiGKzulvHrCppgua', '1');
+INSERT INTO detta_usuarios (email, password, enabled) VALUES ('profesional.cuatro@detta.cl', '$2a$10$5Nivt0GKMRId.174ZI77fudTFfCoiti/hL1xhpiH/QFhvfSXlOgW2', '0');
+
+----------------------------------------------------------------------------------------------------
+-- Tabla: detta_usuarios_roles
+----------------------------------------------------------------------------------------------------
+
+INSERT INTO detta_usuarios_roles (email, role) VALUES ('admin@detta.cl', 'ROLE_ADMIN');
+INSERT INTO detta_usuarios_roles (email, role) VALUES ('profesional.uno@detta.cl', 'ROLE_STAFF');
+INSERT INTO detta_usuarios_roles (email, role) VALUES ('profesional.dos@detta.cl', 'ROLE_STAFF');
+INSERT INTO detta_usuarios_roles (email, role) VALUES ('profesional.tres@detta.cl', 'ROLE_STAFF');
+INSERT INTO detta_usuarios_roles (email, role) VALUES ('profesional.cuatro@detta.cl', 'ROLE_STAFF');
