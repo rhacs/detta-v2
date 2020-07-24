66d3c32eac9a7c5a675f1efa7200158a34926b2e
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-07-23 20:56:14 -0400

    agregados los métodos para responder a un click en tabla y a un botón de acción

--- src/main/webapp/recursos/js/general.js
+++ src/main/webapp/recursos/js/general.js
@@ -8,4 +8,31 @@ $(function() {
         // Enviar formulario
         $('#logout').submit();
     });
+
+    // Al presionar una fila de una tabla que esté marcada con data-member
+    $('[data-member]').on('click', function() {
+        // Obtener el miembro
+        let miembro = $(this).data('member');
+
+        // Obtener el identificador
+        let id = $(this).data('id');
+
+        // Redireccionar
+        $(location).attr('href', '/detta/' + miembro + '/' + id);
+    });
+
+    // Al presionar un botón de acción
+    $('[data-action]').on('click', function() {
+        // Obtener acción
+        let accion = $(this).data('action');
+
+        // Verificar acción
+        if (accion === 'agregar') {
+            // Obtener tipo
+            let tipo = $(this).data('type');
+
+            // Redireccionar
+            $(location).attr('href', '/detta/' + tipo + '/agregar');
+        }
+    });
 });
