81775564d8d749ce12371528c4473096d44fbed8
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-07-25 18:16:25 -0400

    método GET verDetalles implementado

--- src/main/java/cl/leid/detta/controladores/AccidentesController.java
+++ src/main/java/cl/leid/detta/controladores/AccidentesController.java
@@ -1,6 +1,5 @@
 package cl.leid.detta.controladores;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -12,6 +11,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -47,7 +47,7 @@ public class AccidentesController {
     // -----------------------------------------------------------------------------------------
 
     /**
-     * Muestra el listado de accidentes según el tipo de usuario
+     * Muestra el listado de {@link Accidente}s según el rol del {@link Usuario}
      * 
      * @param request objeto {@link HttpServletRequest} con la información de la
      *                solicitud que le envía el cliente al {@link HttpServlet}
@@ -89,6 +89,68 @@ public class AccidentesController {
 
         // Agregar listado a la vista
         vista.addObject("accidentes", accidentes);
+
+        // Agregar título
+        vista.addObject("titulo", messageSource.getMessage("titles.accidents", null, locale));
+
+        // Devolver vista
+        return vista;
+    }
+
+    /**
+     * Muestra los detalles de un {@link Accidente}
+     * 
+     * @param id      identificador numérico del {@link Accidente}
+     * @param request objeto {@link HttpServletRequest} con la información de la
+     *                solicitud que le envía el cliente al {@link HttpServlet}
+     * @param auth    objeto {@link Authentication} con la información del
+     *                {@link Usuario} autenticado
+     * @param locale  objeto {@link Locale} con la información regional del cliente
+     * @return un objeto {@link ModelAndView} con la respuesta
+     */
+    @GetMapping(path = "/{id}")
+    public ModelAndView verDetalles(@PathVariable int id, HttpServletRequest request, Authentication auth,
+            Locale locale) {
+        // Crear vista
+        ModelAndView vista = new ModelAndView("accidentes/detalles");
+
+        // Inicializar repositorios
+        ProfesionalesRepositorio profesionalesRepositorio = new ProfesionalesRepositorio(jdbcTemplate);
+        ClientesRepositorio clientesRepositorio = new ClientesRepositorio(jdbcTemplate);
+        AccidentesRepositorio accidentesRepositorio = new AccidentesRepositorio(jdbcTemplate);
+
+        // Buscar información del accidente
+        Accidente accidente = accidentesRepositorio.buscarPorId(id);
+
+        // Verificar si no existe
+        if (accidente == null) {
+            // Redireccionar
+            return new ModelAndView("redirect:/accidentes", "noid", id);
+        }
+
+        // Verificar rol del usuario
+        if (request.isUserInRole("ROLE_STAFF")) {
+            // Obtener información del profesional
+            Profesional profesional = profesionalesRepositorio.buscarPorEmail(auth.getName());
+
+            // Verificar que el accidente le pertenezca al profesional
+            if (!accidente.getProfesionalId().equals(profesional.getId())) {
+                // Redireccionar
+                return new ModelAndView("redirect:/accidentes", "perm", true);
+            }
+        } else if (request.isUserInRole("ROLE_CLIENT")) {
+            // Obtener información del cliente
+            Cliente cliente = clientesRepositorio.buscarPorEmail(auth.getName());
+
+            // Verificar que el accidente le pertenezca al cliente
+            if (accidente.getClienteId() != cliente.getId()) {
+                // Redireccionar
+                return new ModelAndView("redirect:/accidentes", "perm", true);
+            }
+        }
+
+        // Agregar accidente a la vista
+        vista.addObject("accidente", accidente);
 
         // Agregar título
         vista.addObject("titulo", messageSource.getMessage("titles.accidents", null, locale));
