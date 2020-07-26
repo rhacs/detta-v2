8922ef745b1e76dbedee3dd59f0fc75d73f6117a
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-07-26 18:20:28 -0400

    clase Constantes implementada

---
+++ src/main/java/cl/leid/detta/Constantes.java
@@ -0,0 +1,44 @@
+package cl.leid.detta;
+
+public class Constantes {
+
+    // Tablas
+    // -----------------------------------------------------------------------------------------
+
+    /** Tabla con la información de los {@link Usuario}s */
+    public static final String TABLA_USUARIOS = "detta_usuarios";
+
+    /** Tabla con la información de los roles de los {@link Usuario}s */
+    public static final String TABLA_ROLES = "detta_usuarios_roles";
+
+    /** Tabla con la información de los {@link Profesional}es */
+    public static final String TABLA_PROFESIONALES = "detta_profesionales";
+
+    /** Tabla con la información de los {@link Cliente}s */
+    public static final String TABLA_CLIENTES = "detta_clientes";
+
+    /** Tabla con la información de los {@link Accidente}s */
+    public static final String TABLA_ACCIDENTES = "detta_accidentes";
+
+    /** Tabla con la información de las {@link Accion}es */
+    public static final String TABLA_ACCIONES = "detta_acciones";
+
+    // Secuencias
+    // -----------------------------------------------------------------------------------------
+
+    /** Secuencia generadora de IDs para {@link Usuario} */
+    public static final String SECUENCIA_USUARIOS = "detta_usuarios_sq";
+
+    /** Secuencia generadora de IDs para {@link Profesional} */
+    public static final String SECUENCIA_PROFESIONALES = "detta_profesionales_sq";
+
+    /** Secuencia generadora de IDs para {@link Cliente} */
+    public static final String SECUENCIA_CLIENTES = "detta_clientes_sq";
+
+    /** Secuencia generadora de IDs para {@link Accidente} */
+    public static final String SECUENCIA_ACCIDENTES = "detta_accidentes_sq";
+
+    /** Secuencia generadora de IDs para {@link Accion} */
+    public static final String SECUENCIA_ACCIONES = "detta_acciones_sq";
+
+}
