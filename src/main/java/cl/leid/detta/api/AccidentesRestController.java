a16bb3b1caaa00e523e009baeeb93b043349c30b
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-04 17:42:49 -0400

    PUT method editarRegistro implemented
    DELETE method eliminarRegistro implemented

--- src/main/java/cl/leid/detta/api/AccidentesRestController.java
+++ src/main/java/cl/leid/detta/api/AccidentesRestController.java
@@ -2,6 +2,7 @@ package cl.leid.detta.api;
 
 import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -14,9 +15,11 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -101,13 +104,63 @@ public class AccidentesRestController {
      * @param accidente objeto {@link Accidente} con la información a agregar
      * @return un objeto {@link ResponseEntity} con la respuesta a la solicitud
      */
-    @PostMapping
+    @PostMapping(path = "/new")
     public ResponseEntity<Accidente> agregarRegistro(@RequestBody @Valid Accidente accidente) {
         // Guardar accidente en el repositorio
         Accidente savedAccidente = accidentesRepositorio.save(accidente);
 
         // Crear y mostrar respuesta
         return ResponseEntity.status(HttpStatus.CREATED).body(savedAccidente);
+    }
+
+    // Solicitudes PUT
+    // -----------------------------------------------------------------------------------------
+
+    /**
+     * Edita la información de un {@link Accidente}
+     * 
+     * @param accidente objeto {@link Accidente} con la información a editar
+     * @param locale    objeto {@link Locale} con la información regional del
+     *                  cliente
+     * @return un objeto {@link ResponseEntity} con la respuesta a la solicitud
+     */
+    @PutMapping(path = "/edit")
+    public ResponseEntity<Accidente> editarRegistro(@RequestBody @Valid Accidente accidente, Locale locale) {
+        // Guardar cambios
+        accidentesRepositorio.save(accidente);
+
+        // Crear y mostrar respuesta
+        return ResponseEntity.status(HttpStatus.OK).body(accidente);
+    }
+
+    // Solicitudes DELETE
+    // -----------------------------------------------------------------------------------------
+
+    /**
+     * Elimina un registro de {@link Accidente} del repositorio
+     * 
+     * @param id     identificador numérico del {@link Accidente}
+     * @param locale objeto {@link Locale} con la información regional del cliente
+     * @return un objeto {@link ResponseEntity} con la respuesta a la solicitud
+     */
+    @DeleteMapping(path = "/{id:\\d+}/del")
+    public ResponseEntity<Accidente> eliminarRegistro(@PathVariable int id, Locale locale) {
+        // Buscar información del Accidente
+        Optional<Accidente> accidente = accidentesRepositorio.findById(id);
+
+        // Verificar si existe
+        if (accidente.isPresent()) {
+            // Eliminar registro
+            accidentesRepositorio.delete(accidente.get());
+
+            // Depuración
+            logger.info("[API] Accidente eliminado: {}", accidente.get());
+
+            // Devolver objeto eliminado
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(accidente.get());
+        }
+
+        throw new InformationNotFoundException(messageSource.getMessage("api.notfound", new Object[] { id }, locale));
     }
 
 }
