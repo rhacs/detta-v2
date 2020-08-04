e22051f70c81bb271f7edb1c42f4674d2c7d2328
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-04 12:03:02 -0400

    POST method procesarFormulario implemented

--- src/main/java/cl/leid/detta/controladores/VisitasController.java
+++ src/main/java/cl/leid/detta/controladores/VisitasController.java
@@ -2,13 +2,17 @@ package cl.leid.detta.controladores;
 
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import cl.leid.detta.modelos.Asesoria;
@@ -118,6 +122,52 @@ public class VisitasController {
 
             // Mostrar formulario
             return "asesorias/visitas.formulario";
+        }
+
+        // Redireccionar
+        return "redirect:/asesorias?noid=" + as;
+    }
+
+    // Solicitudes POST
+    // -----------------------------------------------------------------------------------------
+
+    /**
+     * Procesa el formulario cuando el cliente agrega/edita una {@link Visita}
+     * 
+     * @param as            identificador numérico de la {@link Asesoria}
+     * @param id            identificador numérico de la {@link Visita}
+     * @param visita        objeto {@link Visita} con la información a
+     *                      agregar/editar
+     * @param bindingResult objeto {@link BindingResult} con los errores de
+     *                      validación
+     * @param model         objeto {@link Model} con el modelo de la vista
+     * @return un objeto {@link String} con la respuesta a la solicitud
+     */
+    @PostMapping(path = { "/agregar", "/{id:\\d+}/editar" })
+    public String procesarFormulario(@PathVariable int as, @PathVariable Optional<Integer> id, @Valid Visita visita,
+            BindingResult bindingResult, Model model) {
+        // Obtener información de la asesoria
+        Optional<Asesoria> asesoria = asesoriasRepositorio.findById(as);
+
+        // Verificar si existe
+        if (asesoria.isPresent()) {
+            // Verificar si hay errores
+            if (bindingResult.hasErrors()) {
+                // Mostrar formulario
+                return "asesorias/visitas.formulario";
+            }
+
+            // Guardar/editar registro
+            visita = visitasRepositorio.save(visita);
+
+            // Depuración
+            if (id.isPresent())
+                logger.info("[WEB] Se ha editado una Visita: {}", visita);
+            else
+                logger.info("[WEB] se ha agregado una nueva Visita: {}", visita);
+
+            // Redireccionar
+            return "redirect:/asesorias/" + as + "/visitas/" + visita.getId();
         }
 
         // Redireccionar
