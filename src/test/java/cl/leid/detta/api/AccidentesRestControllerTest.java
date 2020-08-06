70e5f92d8d8724d524be750d1fb17101bf5b2400
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-05 20:47:16 -0400

    test AccidentesRestControllerTest implemented

---
+++ src/test/java/cl/leid/detta/api/AccidentesRestControllerTest.java
@@ -0,0 +1,44 @@
+package cl.leid.detta.api;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/servlet-context.xml",
+        "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
+@WebAppConfiguration
+class AccidentesRestControllerTest {
+
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        mvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    void verListadoShouldReturnAnArray() throws Exception {
+        mvc.perform(get("/api/accidentes").contentType(MediaType.APPLICATION_JSON)).andDo(print())
+                .andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray()).andExpect(jsonPath("$[1].id", is(121)));
+    }
+
+}
