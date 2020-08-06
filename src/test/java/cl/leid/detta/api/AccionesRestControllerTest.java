75d8671b466363113949a98ec6460030304c5e69
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-06 16:40:29 -0400

    test AccionesRestControllerTest implemented

---
+++ src/test/java/cl/leid/detta/api/AccionesRestControllerTest.java
@@ -0,0 +1,48 @@
+package cl.leid.detta.api;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/servlet-context.xml",
+        "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
+@WebAppConfiguration
+class AccionesRestControllerTest {
+
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        mvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    void verListadoTest() throws Exception {
+        mvc.perform(get("/api/acciones").contentType(MediaType.APPLICATION_JSON)).andDo(print())
+                .andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void verDetallesTest() throws Exception {
+        mvc.perform(get("/api/acciones/{id}", 1).contentType(MediaType.APPLICATION_JSON)).andDo(print())
+                .andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.detalles").value("Eliminó una capacitación: 1"));
+    }
+
+}
